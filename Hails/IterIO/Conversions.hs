1499b95e7ca2b214982fed700875998ccc8b1cdd
Deian Stefan <deian@cs.stanford.edu>
2012-03-05 00:44:29 -0800

    completed def of iterIOtoIterLIO
    make sure Conversations is unsafe
    
    minor changes
    need to double check onumIO to OnumLIO

--- Hails/IterIO/Conversions.hs
+++ Hails/IterIO/Conversions.hs
@@ -1,4 +1,11 @@
-module Hails.IterIO.Conversions (iterIOtoIterLIO, onumIOtoOnumLIO) where
+{-# LANGUAGE CPP #-}
+#if __GLASGOW_HASKELL__ >= 704
+{-# LANGUAGE Unsafe #-}
+#endif
+module Hails.IterIO.Conversions ( iterIOtoIterLIO
+                                , ioIterRtoLIO
+                                , onumIOtoOnumLIO
+                                ) where
 
 import qualified Data.ByteString.Lazy.Char8 as L
 import Data.IterIO
@@ -6,15 +13,22 @@ import LIO.TCB
 
 type L = L.ByteString
 
-iterIOtoIterLIO :: (LabelState l p s, ChunkData a) => Iter a IO b -> Iter a (LIO l p s) b
-iterIOtoIterLIO iterIn = Iter $ \c -> go $ (runIter iterIn) c
-  where go (Done a (Chunk b c)) = Done a (Chunk b c)
-        go (IterM m) = do
-          let m1 = fmap go $ ioTCB m
-          IterM m1
-        go (IterF next) = IterF $ iterIOtoIterLIO next
-        go (Fail itf ma mb) = Fail itf ma mb
-        go _ = undefined -- TODO: complete implementation
+-- | Lift  the underlying monad of an 'Iter' from 'IO' to 'LIO'.
+iterIOtoIterLIO :: (LabelState l p s, ChunkData t)
+                => Iter t IO a -> Iter t (LIO l p s) a
+iterIOtoIterLIO iterIn = Iter $ \c -> ioIterRtoLIO $ (runIter iterIn) c
 
+-- | Lift  the underlying monad of an 'IterR' from 'IO' to 'LIO'.
+ioIterRtoLIO :: (LabelState l p s, ChunkData t)
+             => IterR t IO a -> IterR t (LIO l p s) a
+ioIterRtoLIO (Done a (Chunk b c)) = Done a (Chunk b c)
+ioIterRtoLIO (IterM m) = IterM $ fmap ioIterRtoLIO $ rtioTCB m
+ioIterRtoLIO (IterF next) = IterF $ iterIOtoIterLIO next
+ioIterRtoLIO (Fail itf ma mb) = Fail itf ma mb
+ioIterRtoLIO (IterC (CtlArg carg f c)) =
+  let g = \cres -> iterIOtoIterLIO $ f cres
+  in IterC (CtlArg carg g c)
+
+-- | Lift the underlying monad of an 'Inum' from 'IO' to 'LIO'.
 onumIOtoOnumLIO :: LabelState l p s => Onum L IO L -> Onum L (LIO l p s) a
-onumIOtoOnumLIO inO = mkInum $ iterIOtoIterLIO (inO .|$ dataI)
+onumIOtoOnumLIO inO = mkInum $ iterIOtoIterLIO (inO .| dataI)
