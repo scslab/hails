f5d5bb48128b52bdab2ad0c2726cdf5e3ec6c668
Amit Aryeh Levy <alevy@chelsea.scs.stanford.edu>
2012-03-05 21:27:21 -0800

    Make a class out of policies and allow group declassification

--- Hails/Database.hs
+++ Hails/Database.hs
@@ -1,49 +1,71 @@
-{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE Trustworthy, ScopedTypeVariables #-}
 
-module Hails.Database ( withDB, databases ) where
+module Hails.Database ( mkPolicy, withDB ) where
 
+import Data.Typeable
 import LIO.MonadCatch
 import LIO.DCLabel
+import DCLabel.Core
 import Data.String.Utils
-import Hails.TCB.Load ( loadDatabase )
-import Hails.Database.MongoDB (DCAction)
 import Hails.Database.MongoDB.TCB.Types
-import Hails.Database.MongoDB.TCB.DCAccess (dcAccess, policyDB)
-import Database.MongoDB.Query (Failure(..) )
+import Hails.Database.MongoDB.TCB.DCAccess
 import qualified Data.UString as U
 import System.Environment
 import System.IO.Unsafe
 import qualified Data.ByteString.Char8 as C
 
+-- | Given a principal corresponding to the databaes owner and a
+-- database name create the corresponding database object in @LIO@.
+loadDatabase :: DatabasePolicy dbp
+             => Principal
+             -> DatabaseName
+             -> (DC dbp)
+loadDatabase dbPrincipal dbName = do
+    let policyPriv = createPrivTCB $ newPriv dbPrincipal
+    let dbConf = DBConf dbName policyPriv
+    createDatabasePolicy dbConf
+
+-- | Create a @DatabasePolicy@ with the appropriate underline databse
+-- name and privileges, determined by the actual instance requested.
+mkPolicy :: forall dbp. (DatabasePolicy dbp, Typeable dbp) => DC dbp
+mkPolicy = do
+  let tpc = undefined :: dbp
+  let tp = typeRepTyCon $ typeOf $ tpc
+  let typeName = tyConPackage tp ++ ":" ++ tyConModule tp ++
+                  "." ++ tyConName tp
+  maybe err doit $ lookup typeName databases
+    where doit (dbName, dbPrincipal) = do
+            result <- loadDatabase dbPrincipal dbName
+            return $ result
+          err = throwIO . userError $ "confLineToDBPair: could not parse line"
+
 -- | Get the DB pair from a configuration line.
-confLineToDBPair :: String
-                 -> IO (DatabaseName, DC (Database DCLabel))
-confLineToDBPair line = do
-  case split ":" line of
-    (dbPrincipal:dbName:policyMod:file:[]) -> do
-      let dbP = principal . C.pack $ dbPrincipal
-          dbN = U.pack dbName
-      dbp <- loadDatabase dbP dbN policyMod file
-      let db = fmap policyDB dbp
-      return (dbN, db)
-    _ -> throwIO . userError $ "confLineToDBPair: could not parse line"
+confLineToConfPair :: String
+                   -> (String, (DatabaseName, Principal))
+confLineToConfPair line = do
+  case split "\t" line of
+    (typeName:dbPrincipal:dbName:[]) -> (typeName, (dbN, dbP))
+      where dbP = principal . C.pack $ dbPrincipal
+            dbN = U.pack dbName
+    _ -> ("",(undefined, undefined))
 
 -- | Get all the databases in the system.
-databases :: [(DatabaseName, DC (Database DCLabel))]
+databases :: [(String, (DatabaseName, Principal))]
 databases = unsafePerformIO $ do
   env <- getEnvironment
   let configFile = maybe "/etc/share/hails/conf/databases.conf" id
                       (lookup "DATABASE_CONFIG_FILE" env)
   confLines <- fmap (split "\n") $ readFile configFile
-  mapM confLineToDBPair $ filter ((> 0) . length) confLines
+  return $ map confLineToConfPair $ filter ((> 0) . length) confLines
+
 
 -- | Given a database name and a database action, execute the action
 -- on the database.
-withDB :: DatabaseName
+withDB :: DatabasePolicy dbp
+       => dbp
        -> DCAction a
        -> DC (Either Failure a)
-withDB dbName act = do
-  maybe err doit $ lookup dbName databases
-    where doit getDB = getDB >>= \db -> dcAccess db act
-          err = return . Left $ QueryFailure (-1) "No such database"
+withDB dbp act = do
+  let db = policyDB dbp
+  dcAccess db act
 
