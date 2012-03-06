530fc30e76b19e03743f9a75d4082a19dc42b076
Deian Stefan <deian@cs.stanford.edu>
2012-03-05 23:43:07 -0800

    use new conversions & leave authorization header

--- Hails/IterIO/Conversions.hs
+++ Hails/IterIO/Conversions.hs
@@ -5,18 +5,23 @@
 module Hails.IterIO.Conversions ( iterIOtoIterLIO
                                 , ioIterRtoLIO
                                 , onumIOtoOnumLIO
+                                , inumIOtoInumLIO 
                                 ) where
 
 import qualified Data.ByteString.Lazy.Char8 as L
+import Data.IORef
 import Data.IterIO
+import LIO.DCLabel
 import LIO.TCB
+import Control.Monad
+import Control.Monad.Trans
 
 type L = L.ByteString
 
 -- | Lift  the underlying monad of an 'Iter' from 'IO' to 'LIO'.
 iterIOtoIterLIO :: (LabelState l p s, ChunkData t)
                 => Iter t IO a -> Iter t (LIO l p s) a
-iterIOtoIterLIO iterIn = Iter $ \c -> ioIterRtoLIO $ (runIter iterIn) c
+iterIOtoIterLIO = adaptIterM rtioTCB
 
 -- | Lift  the underlying monad of an 'IterR' from 'IO' to 'LIO'.
 ioIterRtoLIO :: (LabelState l p s, ChunkData t)
@@ -32,3 +37,52 @@ ioIterRtoLIO (IterC (CtlArg carg f c)) =
 -- | Lift the underlying monad of an 'Inum' from 'IO' to 'LIO'.
 onumIOtoOnumLIO :: LabelState l p s => Onum L IO L -> Onum L (LIO l p s) a
 onumIOtoOnumLIO inO = mkInum $ iterIOtoIterLIO (inO .| dataI)
+
+
+-- | Lift the underlying monad of the Iter and IterR result type.
+inumResIOtoIO :: (LabelState l p s, ChunkData tIn, ChunkData tOut)
+              => Iter tIn IO (IterR tOut IO a)
+              -> Iter tIn (LIO l p s) (IterR tOut (LIO l p s) a)
+inumResIOtoIO iIn = liftM ioIterRtoLIO $ iterIOtoIterLIO iIn
+
+-- | Run an LIO computation.
+iterLIOtoIterIO :: (LabelState l p s, ChunkData t)
+                => Iter t (LIO l p s) a
+                -> LIOstate l p s
+                -> Iter t IO (a, LIOstate l p s)
+iterLIOtoIterIO iter0 s0 = adaptIter (\a -> (a, s0)) adapt iter0
+    where adapt m = lift (runLIO m s0) >>= uncurry iterLIOtoIterIO
+-- iterLIOtoIterIO lio = runStateTLI (adaptIterM peelLIO lio)
+--  where peelLIO (LIO x) = x
+
+{-
+(Iter tOut IO a -> Iter tIn IO (IterR tOut IO a)) -> 
+(Iter tOut DC a -> Iter tIn DC (IterR tOut DC a))
+-}
+
+inumIOtoInumLIO :: (ChunkData tIn, ChunkData tOut)
+    => Inum tIn tOut IO a
+    -> LIOstate DCLabel TCBPriv ()
+    -> Inum tIn tOut DC a
+inumIOtoInumLIO io12 s0 = \dc1 -> do
+  -- Create ref that will be used to store the final state after
+  -- running dc1
+  ref <- lift $ ioTCB $ newIORef s0 
+  -- run dc1
+  let io1 = do (x, s1) <- iterLIOtoIterIO dc1 s0 {- Iter tOut IO (a, LIOstate) -}
+  -- save end state
+               liftIO $ writeIORef ref s1
+               return x
+  -- apply io1 to the inum
+      io2  = io12 io1                            {- Iter tIn  IO (IterR tOut IO a) -}
+  -- change the monad from IO to DC
+  res <- inumResIOtoIO io2                       {- Iter tIn  DC (IterR tOut DC a) -}
+  -- set the end label to the end of that running dc1
+  lift $ do s1 <- ioTCB $ readIORef ref
+            putTCB s1
+  return res
+
+{-
+(Iter tOut IO a -> Iter () IO (IterR tOut IO a)) ->
+(Iter tOut DC a -> Iter () IO (IterR tOut DC a)) ->
+-}
