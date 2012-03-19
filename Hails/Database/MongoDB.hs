d0113c7ceab9b40409f06716b2d7c62c5e566583
Deian Stefan <deian@cs.stanford.edu>
2012-03-19 02:17:58 -0700

    forgot to export non-P to Safe

--- Hails/Database/MongoDB.hs
+++ Hails/Database/MongoDB.hs
@@ -26,11 +26,13 @@ module Hails.Database.MongoDB ( module Hails.Data.LBson
                               , DatabasePolicy(..)
                               , relabelGroupsP, relabelGroupsSafe
                               -- * Query
-                              , insert, insert_, insertP, insertP_, saveP
-                              , findP
-                              , findOneP
-                              , select
+                              , insert, insert_
+                              , insertP, insertP_
+                              , save, saveP
+                              , find, findP
+                              , findOne, findOneP
                               , next, nextP
+                              , select
                               -- * Misc
                               , Failure
                               ) where
@@ -38,5 +40,5 @@ module Hails.Database.MongoDB ( module Hails.Data.LBson
 import Hails.Database.MongoDB.TCB.Types
 import Hails.Database.MongoDB.TCB.Query
 import Hails.Database.MongoDB.TCB.DCAccess
-import Hails.Data.LBson hiding (sort)
+import Hails.Data.LBson hiding (sort, find)
 
