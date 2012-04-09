269fc15578a7c953cb665ecd7bcf7dbbe253d540
Amit Aryeh Levy <amit@amitlevy.com>
2012-04-09 14:21:42 -0700

    Convert 'field[]=blah&field[]=bloo' to field = [blah, bloo] in bson

--- Hails/Database/MongoDB/TCB/Convert.hs
+++ Hails/Database/MongoDB/TCB/Convert.hs
@@ -6,10 +6,14 @@ module Hails.Database.MongoDB.TCB.Convert ( -- * Converting HTTP requests
 
 import LIO
 import LIO.TCB
+import qualified Data.Bson as B (Value(..))
+import qualified Data.UString as U
 import Data.IterIO
 import Data.IterIO.Http
 import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.ByteString.Char8 as S
+import Data.List
+import Data.List.Utils
 import Data.UString (pack)
 import Hails.Data.LBson.TCB
 
@@ -33,6 +37,18 @@ formFolder :: (LabelState l p s)
 formFolder req = foldForm req docontrol []
   where docontrol acc field = do
           formVal <- fmap L.unpack pureI
-          let lfld = pack (S.unpack.ffName $ field) =: formVal
-          return $ lfld : acc
+          let k = S.unpack $ ffName field
+          case k of
+            _ | endswith "[]" k -> return $ appendVal acc k formVal
+              | otherwise -> do
+              let lfld = pack (S.unpack.ffName $ field) =: formVal
+              return $ lfld : acc
 
+appendVal :: LabelState l p s => [Field l] -> String -> String -> [Field l]
+appendVal doc k' formVal =
+  let k = U.pack $ takeWhile (/= '[') k'
+      field = (k := BsonVal (B.Array [B.String $ U.pack formVal]))
+  in case find (isKey k) doc of
+      Just (_ := (BsonVal (B.Array arr))) -> (k =: ((B.String $ U.pack formVal):arr)):(deleteBy (\x y -> (key x) == (key y)) field doc)
+      _ -> field:doc
+  where isKey kk (k := _) = k == kk
