ccc740fbe8e5fb38f82059d70dc860905c4f27e3
Deian Stefan <deian@cs.stanford.edu>
2012-08-25 11:48:15 -0700

    update tests

--- tests/AuthTests.hs
+++ tests/AuthTests.hs
@@ -14,7 +14,7 @@ tests = [authTest]
 authTest :: Test
 authTest = testGroup "Auth"
   [ testCase "Require Login on X-Hails-Login header" $ runSession (do
-        resp <- request undefined
+        resp <- request getTop
         assertHeader "TestHeader" "MyHeaderVal" resp) $
       requireLoginMiddleware (return $ responseLBS status301 [("TestHeader", "MyHeaderVal")] "") $
       const . return $ responseLBS status401 [("x-hails-login", "yes")] ""
@@ -25,3 +25,18 @@ authTest = testGroup "Auth"
       const . return $ responseLBS status200 [] ""
   ]
 
+-- | Simple get request
+getTop :: Request
+getTop = Request { requestMethod = methodGet
+                 , httpVersion = http11
+                 , rawPathInfo = ""
+                 , rawQueryString = ""
+                 , serverName = "locahost"
+                 , serverPort = 8080
+                 , requestHeaders = []
+                 , isSecure = False
+                 , remoteHost = undefined
+                 , pathInfo = []
+                 , queryString = []
+                 , requestBody = undefined
+                 , vault = undefined }
