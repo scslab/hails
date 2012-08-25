3861e539596e1171eb9176c01b92dbaf97d1ed6b
Deian Stefan <deian@cs.stanford.edu>
2012-08-24 20:29:31 -0700

    refactor http server stuff

--- Hails/HttpServer.hs
+++ Hails/HttpServer.hs
@@ -1,89 +1,263 @@
+{-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE OverloadedStrings #-}
 {- |
 
-This module exports methods for constructing a WAI-based HTTP server to serve
-Hails 'Application's.
+This module exports the core of the Hails HTTP server. Specifically it
+defines basic types, such as HTTP 'Request' and 'Response', used by
+the Hails web server and untrusted Hails 'Application's. 
+
+At a high level, a Hails 'Application', is a function from 'Request'
+to 'Response' in the 'DC' monad. Every application response is
+sanitized and sanity checked with the 'secureApplication'
+'Middleware'.
+
+Hails uses Wai, and as such we provide two functions for converting
+Hails 'Application's to Wai 'W.Applicatoin's: '
+'devHailsApplication' used to execute Hails apps in development
+mode, and 'hailsApplicationToWai' that should be used in production
+with an authentication service from "Hails.HttpServer.Auth".
 
 -}
-module Hails.HttpServer
-  ( devHailsHandler
-  , hailsApplication
+module Hails.HttpServer (
+  -- * Requests
+    Request(..)
+  -- * Responses
+  , Response(..)
+  , addResponseHeader, removeResponseHeader
+  -- * Applications and middleware
+  , Application, RequestConfig(..)
+  , Middleware
+  -- ** Execute Hails application in development mode
+  , devHailsApplication
+  -- ** Execute Hails application
+  , hailsApplicationToWai
+  -- ** Middleware used by Hails
+  , browserLabelGuard
+  , guardSensitiveResp 
+  , sanitizeResp
   ) where
 
-import Control.Monad.IO.Class (liftIO)
+import qualified Data.List as List
+import           Data.Text (Text)
+import qualified Data.ByteString as S
 import qualified Data.ByteString.Char8 as S8
-import Network.HTTP.Types
+import qualified Data.ByteString.Lazy as L
+import           Data.Conduit
+import           Data.Conduit.List
+
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Exception (fromException)
+
+
+import           Network.Socket (SockAddr)
+import           Network.HTTP.Types
+import qualified Network.HTTP.Types as H
+import qualified Network.HTTP.Types.Header as H
 import qualified Network.Wai as W
 
-import Hails.HttpServer.Auth
-import Hails.HttpServer.Types
-import LIO
-import LIO.TCB
-import LIO.DCLabel
-import LIO.DCLabel.Privs.TCB
-import LIO.Labeled.TCB
+import           LIO
+import           LIO.TCB
+import           LIO.DCLabel
+import           LIO.DCLabel.Privs.TCB
+import           LIO.Labeled.TCB
 
--- | Hails 'Middleware' that ensures the 'Response' from the application is
--- readable by the client's browser (as determined by the result label of the
--- app computation and the label of the browser). If the response is not
--- readable by the browser, the middleware sends a 403 (unauthorized) response
--- instead.
-browserGuardMiddleware :: Middleware
-browserGuardMiddleware hailsApp conf req = do
-  putLIOStateTCB $ LIOState { lioLabel = dcPub, lioClearance = browserLabel conf}
+import           Hails.HttpServer.Auth
+
+--
+-- Request
+--
+
+-- | A request sent by the end-user.
+data Request = Request {  
+  -- | HTTP Request (e.g., @GET@, @POST@, etc.).
+  requestMethod  :: H.Method
+  -- | HTTP version (e.g., 1.1 or 1.0).
+  ,  httpVersion    :: H.HttpVersion
+  -- | Extra path information sent by the client.
+  ,  rawPathInfo    :: S.ByteString
+  -- | If no query string was specified, this should be empty. This value
+  -- /will/ include the leading question mark.
+  -- Do not modify this raw value- modify queryString instead.
+  ,  rawQueryString :: S.ByteString
+  -- | Generally the host requested by the user via the Host request header.
+  -- Backends are free to provide alternative values as necessary. This value
+  -- should not be used to construct URLs.
+  ,  serverName     :: S.ByteString
+  -- | The listening port that the server received this request on. It is
+  -- possible for a server to listen on a non-numeric port (i.e., Unix named
+  -- socket), in which case this value will be arbitrary. Like 'serverName',
+  -- this value should not be used in URL construction.
+  ,  serverPort     :: Int
+  -- | The request headers.
+  ,  requestHeaders :: H.RequestHeaders
+  -- | Was this request made over an SSL connection?
+  ,  isSecure       :: Bool
+  -- | The client\'s host information.
+  ,  remoteHost     :: SockAddr
+  -- | Path info in individual pieces- the url without a hostname/port
+  -- and without a query string, split on forward slashes,
+  ,  pathInfo       :: [Text]
+  -- | Parsed query string information
+  ,  queryString    :: H.Query
+  -- | Lazy ByteString containing the request body.
+  ,  requestBody    :: L.ByteString
+  } deriving Show
+
+
+-- | Convert a WAI 'W.Request' to a Hails 'Request' by consuming the body into
+-- a 'L.ByteString'.
+waiToHailsReq :: W.Request -> ResourceT IO Request
+waiToHailsReq req = do
+  body <- fmap L.fromChunks $ W.requestBody req $$ consume
+  return $ Request { requestMethod = W.requestMethod req
+                   , httpVersion = W.httpVersion req
+                   , rawPathInfo = W.rawPathInfo req
+                   , rawQueryString = W.rawQueryString req
+                   , serverName = W.serverName req
+                   , serverPort = W.serverPort req
+                   , requestHeaders = W.requestHeaders req
+                   , isSecure = W.isSecure req
+                   , remoteHost = W.remoteHost req
+                   , pathInfo = W.pathInfo req
+                   , queryString = W.queryString req
+                   , requestBody = body }
+--
+-- Response
+--
+
+-- | A response sent by the app.
+data Response = Response {
+  -- | Response status
+    respStatus :: H.Status
+  -- | Response headers
+  , respHeaders :: H.ResponseHeaders 
+  -- | Response body
+  , respBody :: L.ByteString
+  } deriving Show
+
+-- | Add/replace a 'H.Header' to the 'Response'
+addResponseHeader :: Response -> H.Header -> Response
+addResponseHeader resp hdr@(hname, _) = resp { respHeaders = hdr:headers }
+  where headers = List.filter ((/= hname) . fst) $ respHeaders resp
+
+-- | Remove a header (if it exists) from the 'Response'
+removeResponseHeader :: Response -> H.HeaderName -> Response
+removeResponseHeader resp hname = resp { respHeaders = headers }
+  where headers = List.filter ((/= hname) . fst) $ respHeaders resp
+
+-- | Convert a Hails 'Response' to a WAI 'W.Response'
+hailsToWaiResponse :: Response -> W.Response
+hailsToWaiResponse (Response stat rhd body) = W.responseLBS stat rhd body
+
+
+
+--
+-- Application & middleware
+--
+
+-- | The settings with which the app will run.
+data RequestConfig = RequestConfig {
+  -- | The label of the browser the reponse will be sent to.
+    browserLabel :: DCLabel
+  -- | The label of the incoming request (with the logged in user's integrity).
+  , requestLabel :: DCLabel
+  -- | A privilege minted for the app.
+  , appPrivilege :: DCPriv
+  } deriving Show
+
+-- | Base Hails type implemented by untrusted applications.
+type Application = RequestConfig -> DCLabeled Request -> DC Response
+
+-- | Convenience type for middleware components.
+type Middleware = Application -> Application
+
+-- | Hails 'Middleware' that ensures the 'Response' from the
+-- application is readable by the client's browser (as determined by the
+-- result label of the app computation and the label of the browser). If
+-- the response is not readable by the browser, the middleware sends a
+-- 403 (unauthorized) response instead.
+browserLabelGuard :: Middleware
+browserLabelGuard hailsApp conf req = do
   response <- hailsApp conf req
   resultLabel <- getLabel
-  return $
-    if resultLabel `canFlowTo` (browserLabel conf)
-      then response
-      else Response status403 [] ""
+  return $ if resultLabel `canFlowTo` (browserLabel conf)
+             then response
+             else Response status403 [] ""
 
--- Remove anything from the response that could cause inadvertant\
--- declasification (e.g. Cookies)
+-- | Adds the header @X-Hails-Sensitive: Yes@ to the response if the
+-- label of the computation does not flow to the public label, 'dcPub'
+guardSensitiveResp :: Middleware
+guardSensitiveResp happ p req = do
+  response <- happ p req
+  resultLabel <- getLabel
+  return $ if resultLabel `canFlowTo` dcPub
+            then response
+            else addResponseHeader response ("X-Hails-Sensitive", "Yes")
+
+-- | Remove anything from the response that could cause inadvertant
+-- declasification. Currently this only removes the @Set-Cookie@
+-- header.
 sanitizeResp :: Middleware
 sanitizeResp hailsApp conf req = do
   response <- hailsApp conf req
-  return $ removeResponseHeader response hCookie
+  return $ removeResponseHeader response "Set-Cookie"
   
 
--- | Dumly run a Hails 'Application' and return a corresponding WAI
--- 'W.Applicaiton'. Should generally not be run alone by combined with
--- middleware (e.g. 'browserGuardMiddleware') that ensures the safety of the
--- computation and response.
-transformHailsApp :: Application -> W.Application
-transformHailsApp hailsApp req0 = do
-  hailsRequest <- waiToHailsReq req0
-  let conf = getRequestConf hailsRequest
-  response <- liftIO $ evalDC $ do
-    let lreq = labelTCB (requestLabel conf) hailsRequest
-    hailsApp conf lreq
-  return $ hailsToWaiResponse response
-
--- | Adds the header \"X-Hails-Sensitive: Yes\" to the response if the label of
--- the computation is above 'lpub'.
-addXHailsSensitive :: Middleware
-addXHailsSensitive happ p req = do
-  response <- happ p req
-  resultLabel <- getLabel
-  if resultLabel `canFlowTo` dcPub
-    then return response
-    else return $ addResponseHeader response ("X-Hails-Sensitive", "Yes")
-
 -- | Returns a secure Hails app such that the result 'Response' is guaranteed
--- to be safe to transmit to the client's browser.
+-- to be safe to transmit to the client's browser. The definition is
+-- straight forward from other middleware:
+--
+-- > secureApplication = 'browserLabelGuard'  -- Return 403, if user should not read
+-- >                   . 'guardSensitiveResp' -- Add X-Hails-Sensitive if not public
+-- >                   . 'sanitizeResp'       -- Remove Cookies
 secureApplication :: Middleware
-secureApplication = addXHailsSensitive . browserGuardMiddleware
+secureApplication = browserLabelGuard  -- Return 403, if user should not read
+                  . guardSensitiveResp -- Add X-Hails-Sensitive if not public
+                  . sanitizeResp       -- Remove Cookies
 
--- | Safely wraps a Hails 'Application' in a WAI 'W.Application' that can be
--- run by an application server.
-hailsApplication :: Application -> W.Application
-hailsApplication = transformHailsApp . secureApplication
+--
+-- Executing Hails applications
+--
 
--- | A default Hails handler for development environments. Safely runs a Hails
--- 'Application', using basic HTTP authentication for authenticating users.
--- However, authentication will accept any username/password pair.
-devHailsHandler :: Application -> W.Application
-devHailsHandler = devBasicAuth "Hails" . hailsApplication
+-- | A default Hails handler for development environments. Safely runs
+-- a Hails 'Application', using basic HTTP authentication for
+-- authenticating users.  Note: authentication will accept any
+-- username/password pair, it is solely used to set the user-name.
+devHailsApplication :: Application -> W.Application
+devHailsApplication = devBasicAuth . hailsApplicationToWai
+
+
+-- | Safely wraps a Hails 'Application' in a Wai 'W.Application' that can
+-- be run by an application server. The application is executed with the
+-- 'secureApplication' 'Middleware'. The function returns status 500 if
+-- the Hails application throws an exception and the label of the
+-- exception flows to the browser label (see 'browserLabelGuard'); if the
+-- label does not flow, it responds with a 403.
+hailsApplicationToWai :: Application -> W.Application
+hailsApplicationToWai app0 req0 = do
+  -- Convert request to Hails request
+  hailsRequest <- waiToHailsReq req0
+  -- Extract browser/request configuration
+  let conf = getRequestConf hailsRequest
+  result <- liftIO $ paranoidDC' conf $ do
+    let lreq = labelTCB (requestLabel conf) hailsRequest
+    app conf lreq
+  return $ case result of
+    Right (response,_) -> hailsToWaiResponse response
+    Left err -> case fromException err of
+                  Just (LabeledExceptionTCB l _) -> 
+                    -- as in browserLabelGuard :
+                    if l `canFlowTo` (browserLabel conf)
+                      then resp500 else resp403 
+                  _ -> resp500
+    where app = secureApplication app0
+          resp403 = W.responseLBS status403 [] "" 
+          resp500 = W.responseLBS status500 [] "" 
+          paranoidDC' conf act =
+            paranoidLIO act $ LIOState { lioLabel = dcPub
+                                       , lioClearance = browserLabel conf}
+
+
 
 --
 -- Helper
@@ -101,4 +275,5 @@ getRequestConf req =
       { browserLabel = maybe dcPub (\un -> dcLabel un anybody) userName
       , requestLabel = maybe dcPub (\un -> dcLabel anybody un) userName
       , appPrivilege = appPriv }
+
 
--- Hails/HttpServer/Auth.hs
+++ Hails/HttpServer/Auth.hs
@@ -1,76 +1,59 @@
 {-# LANGUAGE OverloadedStrings #-}
 {- |
 
-Generic definitions for authentication pipelines in Hails.
-'requireLoginMiddleware' looks for the \"X-Hails-Login\" header from an
-'Application's 'Response' and, if present, responds
-to the user with an authentication request instead (e.g. a redirect to a login
-page or an HTTP response with status 401). 
+This module exports generic definitions for Wai-authentication pipelines
+in Hails.  'requireLoginMiddleware' looks for the @X-Hails-Login@
+header from an 'Application' \'s 'Response' and, if present, responds to
+the user with an authentication request instead of the 'Application'
+response (e.g., a redirect to a login page or an HTTP response with
+status 401). 
 
-In addition, authentication middlewares for basic HTTP authentication (useful
-in development environments) and federated (OpenID) authentication. In general,
-authentication middlewares are expected to set the \"X-Hails-User\" header on
-the request if it is from an authenticated user.
+Additionally, this module exports authentication 'Middleware's for basic HTTP
+authentication, 'devBasicAuth', (useful in development environments)
+and federated (OpenID) authentication, 'openIdAuth'. In general,
+authentication 'Middleware's are expected to set the @X-Hails-User@
+header on the request if it is from an authenticated user.
 
 -}
 module Hails.HttpServer.Auth
   ( requireLoginMiddleware
+  -- * Production: OpenID
   , openIdAuth
-  , devBasicAuth, authenticatedBasic
+  -- * Development: basic authentication
+  , devBasicAuth
   ) where
-import Blaze.ByteString.Builder (toByteString)
-import Control.Monad.Trans.Resource
-import Data.Time.Clock
-import Data.ByteString.Base64
-import qualified Data.Text as T
-import Data.Maybe (fromMaybe, isJust, fromJust)
-import qualified Data.ByteString.Char8 as S8
-import Network.HTTP.Conduit (withManager)
-import Network.HTTP.Types
-import Network.Wai
-import Web.Authenticate.OpenId
-import Web.Cookie
 
--- | Helper method for implementing basic authentication. Given a 'Request'
--- returns a username-password pair from the basic authentication header if
--- present and valid.
-authenticatedBasic :: Request -> Maybe (S8.ByteString, S8.ByteString)
-authenticatedBasic req = do
-  case lookup hAuthorization (requestHeaders req) of
-    Nothing -> Nothing
-    Just authStr
-      | not ("Basic" `S8.isPrefixOf` authStr) -> Nothing
-      | otherwise ->
-          let up = fmap (S8.split ':') $ decode $ S8.drop 6 authStr
-          in case up of
-            Right (user:pass:[]) -> Just (user, pass)
-            _ -> Nothing
+import           Blaze.ByteString.Builder (toByteString)
+import           Control.Monad
+import           Control.Monad.Trans.Resource
+import           Data.Time.Clock
+import           Data.ByteString.Base64
+import qualified Data.Text as T
+import           Data.Maybe (fromMaybe, isJust, fromJust)
+import qualified Data.ByteString.Char8 as S8
+import           Network.HTTP.Conduit (withManager)
+import           Network.HTTP.Types
+import           Network.Wai
+import           Web.Authenticate.OpenId
+import           Web.Cookie
 
 
 -- | Basic HTTP authentication middleware for development. Accepts any username
 -- and password.
-devBasicAuth :: String {- ^ Realm -} -> Middleware
-devBasicAuth realm app0 req0 = do
+devBasicAuth :: Middleware
+devBasicAuth app0 req0 = do
   let resp = responseLBS status401
-               [( "WWW-Authenticate"
-                , S8.pack $ "Basic realm=\"" ++ realm ++ "\"")] ""
-  let req = case authenticatedBasic req0 of
+               [( "WWW-Authenticate", "Basic realm=\"Hails development.\"")] ""
+  let req = case getBasicAuthUser req0 of
               Nothing -> req0
-              Just (user, _) ->
-                req0 { requestHeaders = (("X-Hails-User", user):(requestHeaders req0)) }
+              Just user -> req0 { requestHeaders = ("X-Hails-User", user)
+                                                 : requestHeaders req0 }
   requireLoginMiddleware (return resp) app0 req
 
 
-requestToUri :: Request -> S8.ByteString -> S8.ByteString
-requestToUri req path = S8.concat $
-  [ "http"
-  , if isSecure req then "s://" else "://"
-  , serverName req
-  , if serverPort req /= 80 && serverPort req /= 443 then portBS else ""
-  , path ]
-  where portBS = S8.concat [":", S8.pack . show $ serverPort req ]
-
-openIdAuth :: T.Text {- ^ OpenID Provider -} -> Middleware
+-- | Perform OpenID authentication.
+openIdAuth :: T.Text -- ^ OpenID Provider 
+           -> Middleware
 openIdAuth openIdUrl app0 req0 = do
   case pathInfo req0 of
     "_hails":"logout":_ -> do
@@ -113,21 +96,49 @@ openIdAuth openIdUrl app0 req0 = do
                                          , ("Set-Cookie", cookie)] ""
       requireLoginMiddleware redirectResp app0 req
 
--- | If if app responds with header X-Hails-Login, respond with authentication
--- response (Basic Auth, redirect, etc...)
+-- | Executes the app and if the app 'Response' has header
+-- @X-Hails-Login@ and the user is not logged in, respond with an
+-- authentication response (Basic Auth, redirect, etc.)
 requireLoginMiddleware :: ResourceT IO Response -> Middleware
 requireLoginMiddleware loginResp app0 req = do
   appResp <- app0 req
-  case lookup "X-Hails-Login" (responseHeaders appResp) of
-    Nothing -> do
-      return appResp
-    Just _ -> do
-      case lookup "X-Hails-User" (requestHeaders req) of
-        Nothing -> loginResp
-        Just _ -> return appResp
+  if hasLogin appResp && notLoggedIn
+    then loginResp
+    else return appResp
+  where hasLogin r = "X-Hails-Login" `isIn` responseHeaders r
+        notLoggedIn = not $ "X-Hails-User" `isIn` requestHeaders req
+        isIn n xs = isJust $ lookup n xs
 
+-- | Get the hreaders from a response.
 responseHeaders :: Response -> ResponseHeaders
 responseHeaders (ResponseFile _ hdrs _ _) = hdrs
 responseHeaders (ResponseBuilder _ hdrs _) = hdrs
 responseHeaders (ResponseSource _ hdrs _) = hdrs
 
+--
+-- Helpers
+--
+
+-- | Helper method for implementing basic authentication. Given a
+-- 'Request' returns the usernamepair from the basic authentication
+-- header if present.
+getBasicAuthUser :: Request -> Maybe S8.ByteString
+getBasicAuthUser req = do
+  authStr <- lookup hAuthorization $ requestHeaders req
+  unless ("Basic" `S8.isPrefixOf` authStr) $ fail "Not basic auth."
+  let up = fmap (S8.split ':') $ decode $ S8.drop 6 authStr
+  case up of
+     Right (user:_:[]) -> return user
+     _ -> fail "Malformed basic auth header."
+
+-- | Given a request and path, extract the scheme,
+-- hostname and port from the request and createand a URI
+-- @scheme://hostname[:port]/path@.
+requestToUri :: Request -> S8.ByteString -> S8.ByteString
+requestToUri req path = S8.concat $
+  [ "http"
+  , if isSecure req then "s://" else "://"
+  , serverName req
+  , if serverPort req `notElem` [80, 443] then portBS else ""
+  , path ]
+  where portBS = S8.pack $ ":" ++ show (serverPort req)
