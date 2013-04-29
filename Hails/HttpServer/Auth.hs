8649198ba1f7035689b2a8b7689ca86e14f888dd
Amit Aryeh Levy <amit@amitlevy.com>
2013-04-29 01:26:05 -0700

    Nit

--- Hails/HttpServer/Auth.hs
+++ Hails/HttpServer/Auth.hs
@@ -249,8 +249,8 @@ externalAuth key url app req = do
   let mreqAuth = do
         cookieHeaders <- lookup hCookie $ requestHeaders req
         let cookies = parseCookies cookieHeaders
-        mac0 <- lookup "_hails_user_hmac" cookies
-        user <- lookup "_hails_user" cookies
+        mac0 <- fmap (read . S8.unpack) $ lookup "_hails_user_hmac" cookies
+        user <- fmap (read . S8.unpack) $ lookup "_hails_user" cookies
         let mac1 = showDigest $ hmacSha1 key (lazyfy user)
         if S8.unpack mac0 == mac1
           then Just $ req { requestHeaders = ("X-Hails-User", user)
